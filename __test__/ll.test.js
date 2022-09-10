db77a20dd72377e40d70187c68a2d4aa5a8257fa
saeedkokash <saeedkokash@gmail.com>
2022-09-10 21:40:00 +0300

    added a delete method and tested it

--- __test__/ll.test.js
+++ __test__/ll.test.js
@@ -57,3 +57,34 @@ describe('Append to the end of the linked list', () => {
         expect(ll.head.next.next.next).toBeNull();
     })
 })
+
+describe('Delete the middle item of the linked list', () => {
+    
+    test('Delete an empty linked list', () => {
+        const ll = new LinkedList();
+        expect(ll.delete('a')).toEqual('Linked list is empty');
+    })
+       
+    
+    test('delete the middle element of the linked list', () => {
+        const ll = new LinkedList();
+        ll.append('a');
+        ll.append('b');
+        ll.append('c');
+        ll.append('d');
+        ll.append('e');
+
+        // testing the linked list before deleting
+        expect(ll.head.value).toEqual('a');
+        expect(ll.head.next.value).toEqual('b');
+        expect(ll.head.next.next.value).toEqual('c');
+        expect(ll.head.next.next.next.value).toEqual('d');
+        expect(ll.head.next.next.next.next.value).toEqual('e');
+        expect(ll.head.next.next.next.next.next).toBeNull();
+
+        // deleting the middle element
+        ll.delete('c');
+
+    })
+})
+
--- ll.js
+++ ll.js
@@ -30,6 +30,22 @@ class LinkedList {
             
         }
     }
+
+    // delete a node from the linked list
+    delete(val) {
+        if(!this.head) { // this means that linked list is empty
+            return 'Linked list is empty';
+        } else { // this means that linked list is not empty
+            let currentNode = this.head;
+            let previousNode = this.head;
+            while (currentNode.value !== val) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+            previousNode.next = currentNode.next;
+        }
+    }
+
 }
 
 module.exports = LinkedList;
