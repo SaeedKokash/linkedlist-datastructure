7af81984dd580581f4b594356713fd408558b8c7
saeedkokash <saeedkokash@gmail.com>
2022-09-10 20:32:21 +0300

    added append method with tested it

--- ll.js
+++ ll.js
@@ -16,6 +16,20 @@ class LinkedList {
             this.head = node;
         }
     }
+
+    append(val) {
+        const node = new Node(val);
+        if(!this.head) { // this means that linked list is empty
+            this.head = node;
+        } else { // this means that linked list is not empty
+            let currentNode = this.head;
+            while (currentNode.next) {
+                currentNode = currentNode.next
+            }
+            currentNode.next = node;
+            
+        }
+    }
 }
 
 module.exports = LinkedList;
